3750f8f35a9b21873b67aeefab66f9727681be00
Aung Ko Ko Ye <aye@high5hire.com>
2025-04-29 11:45:52 +0100

    fixed dispacth method param issue

--- src/Guzzle/Http/Message/Request.php
+++ src/Guzzle/Http/Message/Request.php
@@ -586,7 +586,7 @@ class Request extends AbstractMessage implements RequestInterface
             // modifying the Event object in your listeners or calling setResponse() on the request
             if ($this->response->isError()) {
                 $event = new Event($this->getEventArray());
-                $this->getEventDispatcher()->dispatch('request.error', $event);
+                $this->getEventDispatcher()->dispatch($event, 'request.error');
                 // Allow events of request.error to quietly change the response
                 if ($event['response'] !== $this->response) {
                     $this->response = $event['response'];
